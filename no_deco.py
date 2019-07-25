a717fcf0b08874622c306e1017da604f8f172f3b
zhangjun <zhangjun@cadg.cn>
2019-07-25 15:12:46 +0800

    this is a program without decorator

---
+++ no_deco.py
@@ -0,0 +1,15 @@
+import time
+def deco(func):
+    start_time=time.time()
+    f()
+    end_time=time.time()
+    excution_time=(end_time-start_time)*1000
+    print("time is {} ms".format(excution_time))
+def f():
+    print("hello")
+    time.sleep(1)
+    print("word")
+if __name__=='__main__':
+    deco(f)
+    print("f.__name__ is {}".format(f.__name__))
+    print()
