332f6235ed0ca1be26662fcd01fa0b0e2d6ac082
zhangjun <zhangjun@cadg.cn>
2019-12-11 18:05:47 +0800

    finish IMBD二分类

--- python/ML/keras/3.4IMBD二分类.py
+++ python/ML/keras/3.4IMBD二分类.py
@@ -2,7 +2,10 @@ from keras.datasets import imdb
 from keras import models
 from keras import layers
 from keras import optimizers
+from keras import losses
+from keras import metrics
 import numpy as np
+import matplotlib.pyplot as plt
 
 (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
 
@@ -35,7 +38,47 @@ model.add(layers.Dense(16, activation='relu'))
 model.add(layers.Dense(1, activation='sigmoid'))
 
 # 编译模型
-model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
+# model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
 
 # 配置优化器
-model.compile(optimizer=optimizers.RMSprop(lr=0.001), loss='binary_crossentropy', metrics=['accuracy'])
+# model.compile(optimizer=optimizers.RMSprop(lr=0.001), loss='binary_crossentropy', metrics=['accuracy'])
+
+# 使用自定义的损失和指标
+# model.compile(optimizer=optimizers.RMSprop(lr=0.001),loss=losses.binary_crossentropy,metrics=[metrics.binary_accuracy])
+
+# 留出验证集
+x_val = x_train[:10000]
+partial_x_train = x_train[10000:]
+y_val = y_train[:10000]
+partial_y_train = y_train[10000:]
+
+# 训练模型
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
+history = model.fit(partial_x_train, partial_y_train, epochs=20, batch_size=512, validation_data=(x_val, y_val))
+
+history_dict = history.history
+# print(history_dict.keys())
+
+# 绘制训练损失和验证损失
+loss_values = history_dict['loss']
+val_loss_values = history_dict['val_loss']
+epochs = range(1, len(loss_values) + 1)
+plt.plot(epochs, loss_values, 'bo', label='Training loss')
+plt.plot(epochs, val_loss_values, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+# 绘制训练进度和验证精度
+# plt.clf()
+acc = history_dict['acc']
+val_acc = history_dict['val_acc']
+plt.plot(epochs, acc, 'ro', label='Training acc')
+plt.plot(epochs, val_acc, 'r', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
